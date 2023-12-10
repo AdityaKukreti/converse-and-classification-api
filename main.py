7d5ab1603e914864917274fa6579850259003091
Aditya Kukreti <74974564+AdityaKukreti@users.noreply.github.com>
2023-12-10 13:00:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask,jsonify,request
 import mediapipe_api
+import converse_api
 
 app = Flask(__name__)
-API = mediapipe_api.MediaPipeAPI()
+MEDIAPIPE_API = mediapipe_api.MediaPipeAPI()
+CONVERSE_API = converse_api.ConverseAPI()
 
 @app.route('/')
 def home():
@@ -11,11 +13,20 @@ def home():
 @app.route('/get-score',methods = ['POST'])
 def sendScorePOST():
     text = request.get_json()['text']
-    return jsonify(API.getScore(text))
+    return jsonify(MEDIAPIPE_API.getScore(text))
 
 @app.route('/get-score/<text>',methods = ['GET'])
 def sendScoreGET(text):
-    return jsonify(API.getScore(text))
+    return jsonify(MEDIAPIPE_API.getScore(text))
+
+@app.route('/get-response', methods = ['POST'])
+def sendResponsePOST():
+    text = request.get_json()['prompt']
+    return jsonify(CONVERSE_API.getResponse(text))
+
+@app.route('/get-response/<text>', methods = ['GET'])
+def sendResponseGET(text):
+    return jsonify(CONVERSE_API.getResponse(text))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port = 10000)
